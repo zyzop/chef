135c82a2f59c0435608ff54527da5088862894fe
Konstantin <admin@sisop.pw>
2017-02-04 15:33:05 +0300

    Update login.rb

--- docker-compose/recipes/login.rb
+++ docker-compose/recipes/login.rb
@@ -1,4 +1,4 @@
 execute "DockerHub login" do
   action :run
-  command "/usr/bin/docker login -e #{node[:DockerHub][:login][:email]} -u #{node[:DockerHub][:login][:username]} -p #{node[:DockerHub][:login][:password]}"
+  command "aws ecr get-login --region eu-west-1"
 end
