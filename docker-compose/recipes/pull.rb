363a208d57d58f57fc8a8a785469eb3e8700157c
Konstantin <admin@sisop.pw>
2017-02-04 15:37:55 +0300

    Update pull.rb

--- docker-compose/recipes/pull.rb
+++ docker-compose/recipes/pull.rb
@@ -1,7 +1,6 @@
 execute 'pull containers images' do
     cwd '/docker-compose/'
-    command 'aws ecr get-login --region eu-west-1'
-    command 'docker-compose pull'
+    command 'aws ecr get-login --region eu-west-1 && docker-compose pull'
     case node[:platform]
     when 'ubuntu'
       environment 'COMPOSE_API_VERSION' => '1.18'
